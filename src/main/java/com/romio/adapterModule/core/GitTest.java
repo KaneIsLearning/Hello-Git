82a74d6caf6070853887c5e4c8682ee1bcc9780a
root <ceshihahufirst@163.com>
2019-05-08 10:31:57 +0800

    commit second

---
+++ src/main/java/com/romio/adapterModule/core/GitTest.java
@@ -0,0 +1,16 @@
+package com.romio.adapterModule.core;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈dd〉
+ *
+ * @author Kane
+ * Date:   2019/5/8 10:29
+ * @since 1.0.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("测试提交文件！");
+    }
+
+}
--- src/main/java/com/romio/adapterModule/core/Test.java
+++ src/main/java/com/romio/adapterModule/core/Test.java
@@ -12,6 +12,7 @@ public class Test {
     public static void main(String[] args) {
         Target target = new Adapter();
         target.request();
+        System.out.println("设计模式--抽象工厂方法");
 
     }
 
