adf0b22f0ba8a706465e2ad43ee463e3d6912445
root <ceshihahufirst@163.com>
2019-05-08 10:40:47 +0800

    commit more two

--- src/main/java/com/romio/adapterModule/core/GitTest.java
+++ src/main/java/com/romio/adapterModule/core/GitTest.java
@@ -11,6 +11,7 @@ package com.romio.adapterModule.core;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("测试提交文件！");
+        System.out.println("测试THRED");
     }
 
 }
