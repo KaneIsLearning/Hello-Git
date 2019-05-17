c62ba45e77b22480eac16be04d3f8b126a23e38c
root <ceshihahufirst@163.com>
2019-05-17 14:41:37 +0800

    add message about procedure

--- src/main/java/com/romio/CommandPattern/client/ClientTest.java
+++ src/main/java/com/romio/CommandPattern/client/ClientTest.java
@@ -19,11 +19,14 @@ import java.util.List;
  */
 public class ClientTest {
     public static void main(String[] args) {
+        /**
+         * 流程：客户点单-》前台（判断）生成订单，通知厨房-》厨房执行订单。
+         */
         Receiver receiver = new Receiver("厨房");
         Invoker invoker = new Invoker(1,"前台管理员");
         List<Command> commands = new ArrayList<>();
         Command command = new CommandImp("烤地瓜",5);
-        Command command1 = new CommandImp("番茄",6);
+        Command command1 = new CommandImp("番茄",10);
         commands.add(command);
         commands.add(command1);
         Order order = invoker.createOrder(101,commands);
